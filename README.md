570aa77be9ede7919cfbaa6be1890efcaecd861e
Michael Angeles <jibbajabba@gmail.com>
2015-07-16 10:18:39 -0700

    Adding gulp tasks for JS and CSS, renaming theme to docs-balsamiq-com.

--- README.md
+++ README.md
@@ -2,14 +2,34 @@
 docs.calsamiq.com
 
 # Usage
+
+## Running Hugo
 - Run launchHugo.sh (or $ hugo server --watch)
 - If page hierarchy has been altered from previous build, remove public/ directory first, then run hugo.
 - To build hugo without watch, just run $ hugo
 
+## Running Gulp to rebuild CSS/JS Assets
+
+### Installing Gulp
+- Make sure you have gulp installed (https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md). If you don't run this:
+    $ sudo npm install -g gulp
+- Or Install gulp in your project devDependencies:
+    $ npm install --save-dev gulp
+- Install dev dependencies by running this from terminal in docs.balsamiq.com directory:
+    $ npm install
+
+### Updating Gulp
+- To update globally: $ npm update gulp -g
+- To update a local instance: cd /your/folder/ then: $ npm update gulp
+
 # TODO
 - [x] Generate nav
 - [x] Create balsamiq theme
-- [ ] Figure out what leon wants to do with the IA
+- [x] Figure out what leon wants to do with the IA
 - [ ] Create Nav and Overview pages based on Leon's IA
+- [ ] Make Titles h1 and decrease headings below that
+- [x] Set up gulp and document
+- [x] Create gulp JS tasks
+- [x] Create gulp CSS tasks
 - [ ] Create gulp tasks to pull header/footer from balsamiq.com repo into partials
 - [ ] Update shell script to handle gulp tasks
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+// LOAD
+var gulp = require("gulp"),
+  util = require("gulp-util"),
+  sass = require("gulp-sass"),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  del = require('del'),
+  watch = require('gulp-watch'),
+  log = util.log;
+
+// TASKS
+
+// Build CSS
+gulp.task("sass", function(){
+  log("Generate CSS files " + (new Date()).toString());
+    gulp.src('src/sass/**/*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer("last 2 version","safari 5", "ie 8", "ie 9"))
+    .pipe(gulp.dest("src/css-tmp"))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('themes/docs-balsamiq-com/static/css'))    .pipe(notify({ message: 'Sass task complete' }));
+});
+
+// Minify JS
+gulp.task('js', function() {
+  log("Minify JS files " + (new Date()).toString());
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('dbc.js'))
+    .pipe(gulp.dest('src/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('themes/docs-balsamiq-com/static/js'))
+    .pipe(notify({ message: 'JS Minify task complete' }));
+});
+
+// Clean
+gulp.task('clean', function(cb) {
+    del(['themes/docs-balsamiq-com/static/css', 'themes/docs-balsamiq-com/static/js'], cb)
+});
+
+// Default
+gulp.task('default', ['clean'], function() {
+    gulp.start('sass', 'js');
+});
+
+// WATCH
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+
+  // Watch .js files
+  gulp.watch('src/js/**/*.js', ['js']);
+
+});
