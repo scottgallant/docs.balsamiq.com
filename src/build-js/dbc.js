b46ab849d5b076ac020f7cde95673d391d6ab216
Michael Angeles <jibbajabba@gmail.com>
2015-08-03 15:32:35 -0700

    Compressed sidebar nav

--- src/build-js/dbc.js
+++ src/build-js/dbc.js
@@ -1,1 +1,14 @@
 // DBC JS
+$(document).ready(function() {
+
+  // bind change event to select
+  $('#docmenu').on('change', function () {
+      var url = $(this).val(); // get selected value
+      if (url) { // require a URL
+          window.location = url; // redirect
+      }
+      return false;
+  });
+
+
+});
