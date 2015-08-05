ef514648a51ebc99b3efcb2512985b7e66723f54
Michael Angeles <jibbajabba@gmail.com>
2015-08-04 17:12:50 -0700

    Nav Affix, Smoothscroll, Expand/Collapse

--- src/build-js/dbc.js
+++ src/build-js/dbc.js
@@ -1,14 +1,37 @@
 // DBC JS
 $(document).ready(function() {
 
-  // bind change event to select
-  $('#docmenu').on('change', function () {
+  // SELECT AND GO
+  $('#doc-menu').on('change', function () {
       var url = $(this).val(); // get selected value
       if (url) { // require a URL
           window.location = url; // redirect
       }
       return false;
   });
+
+  // MENU TOGGLE ICONs
+  $('.menu-toggle').click(function() {
+    $(this).find('.ss-icon').toggleClass("ss-navigatedown ss-navigateright");
+    // e.preventDefault();
+  });
+
+
+  // SMOOTH SCROLL
+  $("#toc-menu a[href^='#']").on('click', function(e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+    // store hash
+    var hash = this.hash;
+    // animate
+    $('html, body').animate({
+     scrollTop: $(hash).offset().top
+    }, 300, function(){
+     // when done, add hash to url
+     // (default click behaviour)
+     window.location.hash = hash;
+    });
+   });
 
 
 });
